5c56327bddf398a88c4acca4b9160d848e18756e
Kate McGee <mcgeehee@gmail.com>
2020-08-03 14:48:52 -0400

    5/14 complete

--- src/03_modules.py
+++ src/03_modules.py
@@ -8,14 +8,29 @@ level operating system functionality.
 import sys
 # See docs for the sys module: https://docs.python.org/3.7/library/sys.html
 
+print(sys.argv[0])
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
+
+# import fileinput
+for arg in sys.argv:
+    print(arg)
+    print(sys.argv)
+    print("This is the name of the script " + sys.argv[0])
+    print("Number of arguments: ", len(sys.argv))
+    print("The arguments are: " , str(sys.argv))
+
+
 
 # Print out the OS platform you're using:
 # YOUR CODE HERE
 
+print(sys.platform)
+
 # Print out the version of Python you're using:
 # YOUR CODE HERE
+
+print(sys.version)
 
 
 import os
@@ -24,8 +39,12 @@ import os
 # Print the current process ID
 # YOUR CODE HERE
 
+print(os.getpid())
+
 # Print the current working directory (cwd):
 # YOUR CODE HERE
+print(os.getcwd())
 
 # Print out your machine's login name
 # YOUR CODE HERE
+print(os.getlogin())
