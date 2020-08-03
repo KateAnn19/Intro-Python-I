6ea396ad96ae60da13a83c5f1a7fe9a1cd038b80
Kate McGee <mcgeehee@gmail.com>
2020-08-03 15:25:16 -0400

    6/14 complete

--- src/04_printing.py
+++ src/04_printing.py
@@ -8,15 +8,25 @@ x = 10
 y = 2.24552
 z = "I like turtles!"
 
+myTuple = (x,y,x)
+
 # Using the printf operator (%), print the following feeding in the values of x,
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
 
-# Use the 'format' string method to print the same thing
-
 format = "x is %s, y is %s,  z is %s!"
 
-print(format % x,y,z)
+print(format % myTuple)
+
+# Use the 'format' string method to print the same thing
+
+
+txt = "X is {0}, y is {1}, z is {2}"
+txt2 = "X is {num1}, y is {num2}, z is {num3}".format(num1 = 20, num2= 30, num3="hello there")
+txt3 = "X is {}, y is {}, z is {}".format(1,2,"howdy!")
+print(txt.format(10, 2.23552, "I like turtles!"))
+print(txt2)
+print(txt3)
 
 
 
